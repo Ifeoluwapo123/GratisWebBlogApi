334ffeccc9084a3ae9027a2ea071bc85e68b415a
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-09-19 21:29:22 +0100

    comment controller impl

---
+++ controller/comment.controller.js
@@ -0,0 +1,105 @@
+const Comment = require("../model/comment.model");
+
+// Create and Save a commnet on a blog post
+exports.create = (req, res) => {
+
+  // Create a comment object
+  const comment = {
+    comment: req.body.comment,
+    postId: req.body.postId,
+  };
+
+  // Save comment in the database
+  Comment.create(comment)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while creating the comment on the blog post.",
+      });
+    });
+};
+
+// Retrieve all comments on a blog posts from the database.
+exports.findAll = (req, res) => {
+  Comment.findAll().then((comments) => {
+    res.json({ comments: comments });
+  });
+};
+
+exports.findOne = (req, res) => {
+  const uuid = req.params.id;
+
+  if (uuid.length !== 36)
+    return res.redirect(req.protocol + "://" + req.get("host"));
+
+  Comment.findByPk(uuid)
+    .then((data) => {
+      res.json({ comment: data || {} });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving blog post with id=" + uuid,
+      });
+    });
+};
+
+// Update a blog post by the id in the request
+exports.update = (req, res) => {
+  const uuid = req.params.id;
+
+  if (uuid.length !== 36)
+    return res.redirect(req.protocol + "://" + req.get("host"));
+
+  Comment.update(req.body, {
+    where: { commentId: uuid },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "comment was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update comment with id=${uuid}. Maybe post was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({
+        message: "Error updating comment with id=" + uuid,
+      });
+    });
+};
+
+// Delete a blog post with the specified id in the request
+exports.delete = (req, res) => {
+  const uuid = req.params.id;
+
+  if (uuid.length !== 36)
+    return res.redirect(req.protocol + "://" + req.get("host"));
+
+  Comment.destroy({
+    where: { commentId: uuid },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "comment was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete comment with id=${uuid}. Maybe post was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete blog post with id=" + uuid,
+      });
+    });
+};
